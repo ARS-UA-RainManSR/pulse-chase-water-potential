161f17be84ec51729d1027603ddfb1f519853567
Jessica Guo <jessicaguo@email.arizona.edu>
2024-01-11 18:23:17 -0700

    Make new plots per Joel's suggestions

--- scripts/04-plot-data-figs.R
+++ scripts/04-plot-data-figs.R
@@ -172,7 +172,7 @@ ggsave(filename = "figures/fig1_raw_ts.png",
        width = 9,
        units = "in")
 
-#### Figure 2: LWP vs. RWC by pulse ####
+#### Figure 2: LWP vs. RWC by pulse, show hysteresis ####
 vpd_plot <- vpd |> 
   filter(period == "day",
          location == "inside") |> 
@@ -180,7 +180,8 @@ vpd_plot <- vpd |>
   rename(vpd_kpa = mean)
 
 
-wp_rwc_plot <- wp_rwc |> 
+wp_rwc_plot <- wp_rwc |>
+  filter(trt_s %in% c("S1", "S2", "S4")) |>  
   # reclassifly pulses as 1 or 2
   mutate(pulse_num2 = case_when(pulse_num %in% c(3, 7, 13) ~ "Aug 14",
                                 pulse_num %in% c(8, 15) ~ "Aug 21") |> 
@@ -188,31 +189,76 @@ wp_rwc_plot <- wp_rwc |>
   left_join(vpd_plot, by = join_by("date_col" == "date"))
 
 
-wp_rwc_plot |> 
-  filter(trt_s %in% c("S1", "S2", "S4")) |> 
-ggplot() +
-  geom_point(aes(x = wp_mpa, y = RWC,
-                 color = vpd_kpa,
-                 shape = pulse_num2)) +
+wp_sum_plot <- wp_rwc_plot |> 
+  group_by(date_col, days_since_pulse, pulse_num2, trt_s, period) |> 
+  summarize(WP_m = mean(wp_mpa, na.rm = TRUE),
+            WP_sd = sd(wp_mpa, na.rm = TRUE),
+            WP_n = sum(length(!is.na(wp_mpa))),
+            RWC_m = mean(RWC, na.rm = TRUE),
+            RWC_sd = sd(RWC, na.rm = TRUE),
+            RWC_n = sum(length(!is.na(RWC)),
+            vpd = mean(vpd_kpa))) 
+
+fig2 <- ggplot() +
+  geom_point(data = wp_rwc_plot,
+             aes(x = wp_mpa, y = RWC,
+                 color = pulse_num2),
+             alpha = 0.15) +
+  geom_errorbarh(data = wp_sum_plot,
+                aes(xmin = WP_m - WP_sd, xmax = WP_m + WP_sd,
+                    y = RWC_m,
+                    color = pulse_num2), alpha = 0.5) +
+  geom_errorbar(data = wp_sum_plot,
+                aes(x = WP_m, 
+                    ymin = RWC_m - RWC_sd, ymax = RWC_m + RWC_sd,
+                    color = pulse_num2), alpha = 0.5) +
+  geom_point(data = wp_sum_plot,
+             aes(x = WP_m, y = RWC_m,
+                 color = pulse_num2),
+             size = 2) +
+  geom_path(data = wp_sum_plot,
+            aes(x = WP_m,
+                y = RWC_m,
+                linetype = pulse_num2),
+            arrow = arrow(type = "closed",
+                          length = unit(0.15, "cm")),
+            show.legend = FALSE) +
   facet_grid(cols = vars(trt_s),
              rows = vars(period),
              space = "free") +
   scale_y_continuous(expression(paste("RWC (g ", g^-1, ")"))) +
   scale_x_continuous(expression(paste(Psi[leaf], " (MPa)"))) +
-  scale_color_gradient("VPD",
-                       high = "coral", low = "mediumblue") +
+  scale_color_manual("Pulse",
+                     values = c("coral","cornflowerblue")) +
   scale_shape_manual("Pulse", values = 16:15) +
   theme_bw(base_size = 14) +
   theme(panel.grid = element_blank(),
-        strip.background = element_blank())
+        strip.background = element_blank(),
+        strip.placement = "outside",
+        legend.title = element_blank(),
+        legend.position = c(0.08, 0.9),
+        legend.text = element_text(size = 8),
+        legend.background = element_rect(fill = NA)) +
+  guides(color = guide_legend(keyheight = 0.75,
+                              override.aes = list(linetype = NULL)))
 
-#### Figure 3: LWP by days since pulse, colored by VPD ####
+
+ggsave("figures/fig2_wp_rwc.png",
+       plot = fig2,
+       height = 4,
+       width = 6,
+       units = "in")
+
+
+#### Figure 3: LWP by days since pulse, with CDE ####
+# Limit to only S1 and S2
 cde_plot <- cde |> 
   filter(pulse_num %in% unique(wp_rwc$pulse_num)) |> 
   # reclassifly pulses as 1 or 2
   mutate(pulse_num2 = case_when(pulse_num %in% c(3, 7, 13) ~ "Aug 14",
                                 pulse_num %in% c(8, 15) ~ "Aug 21") |> 
-           as.factor()) 
+           as.factor()) |> 
+  filter(trt_s != "S4")
 
 wp_sum_plot <- wp_rwc_plot |> 
   group_by(date_col, days_since_pulse, pulse_num2, trt_s, period) |> 
@@ -230,14 +276,14 @@ dup <- wp_sum_plot |>
          pulse_num2 = "Aug 14")
 
 wp_sum_plot2 <- wp_sum_plot |> 
-  bind_rows(dup) 
+  bind_rows(dup) |> 
+  filter(trt_s != "S4")
   # not needed if not coloring by CDE
   # left_join(select(cde, date, trt_s, cde),
   #           by = join_by("date_col" == "date", "trt_s"))
 
 fig3 <- ggplot() +
   geom_vline(xintercept = 0.5,
-             # color = "gray",
              lty = 2) +
   geom_line(data = wp_sum_plot2,
              aes(x = days_since_pulse,
@@ -256,26 +302,23 @@ fig3 <- ggplot() +
              size = 1.5) +
   geom_line(data = cde_plot,
             aes(x = days_since_pulse,
-                y = cde/8 - 5,
-                # linetype = pulse_num2,
-                color = pulse_num2),
+                y = cde/3 - 5, 
+                color = pulse_num2,
+                # linetype = pulse_num2
+                ),
             linetype = 3) +
-  # geom_point(data = cde_plot,
-  #            aes(x = days_since_pulse,
-  #                y = cde/8 - 5),
-  #            size = 1) +
-  facet_grid(cols = vars(trt_s), 
+   facet_grid(cols = vars(trt_s), 
              rows = vars(period),
              scales = "free_x",
              space = "free_x",
              switch = "y") +
   scale_y_continuous(expression(paste(Psi[leaf], " (MPa)")),
-                     sec.axis = sec_axis(~(.+5)*8,
+                     sec.axis = sec_axis(~(.+5)*3,
                                          name = "CDE (kPa)") ) +
   scale_x_continuous("Day of pulse",
                      breaks = unique(wp_sum_plot2$days_since_pulse)) +
   scale_color_manual("Pulse",
-                     values = c("coral","navy")) +
+                     values = c("coral","cornflowerblue")) +
   scale_linetype_manual("Variable", values = c(1,3)) +
   theme_bw(base_size = 12) +
   theme(panel.grid = element_blank(),
@@ -294,7 +337,8 @@ ggsave("figures/fig3_wp_cde.png",
        width = 6,
        units = "in")
 
-#### Figure 4: MD vs. PD, colored by VPD ####
+
+#### Figure 4: MD vs. PD by pulse, with hysteresis arrows ####
 
 wp_wide_plot <- wp_wide |> 
   mutate(pulse_num2 = case_when(pulse_num %in% c(3, 7, 13) ~ "Aug 14",
@@ -379,3 +423,128 @@ ggsave("figures/fig4_md_pd.png",
        height = 3,
        width = 6,
        units = "in")
+
+###### Sandbox figures ######
+
+#### LWP by days since pulse, with CDE ####
+wp_sum_plot <- wp_rwc_plot |> 
+  group_by(date_col, days_since_pulse, pulse_num2, trt_s, period) |> 
+  summarize(WP_m = mean(wp_mpa, na.rm = TRUE),
+            WP_sd = sd(wp_mpa, na.rm = TRUE),
+            WP_n = sum(length(!is.na(wp_mpa))),
+            vpd = mean(vpd_kpa)) 
+
+# need to duplicate day 0 of pulse 2 for S2 as day 7
+dup <- wp_sum_plot |> 
+  filter(trt_s == "S2",
+         days_since_pulse == 0,
+         pulse_num2 == "Aug 21") |> 
+  mutate(days_since_pulse = 7,
+         pulse_num2 = "Aug 14")
+
+wp_sum_plot2 <- wp_sum_plot |> 
+  bind_rows(dup) |> 
+  filter(trt_s != "S4")
+
+# combind vwc_plot and cde
+
+vwc_cde <- vwc_plot |> 
+  left_join(cde, by = join_by("date", "trt_s")) |> 
+  mutate(pulse_num2 = case_when(pulse_num %in% c(3, 7, 13) ~ "Aug 14",
+                                pulse_num %in% c(8, 15) ~ "Aug 21") |> 
+           as.factor()) |> 
+  filter(!is.na(pulse_num2)) |> 
+  rename("interval" = "period")
+
+ggplot() +
+  geom_vline(xintercept = 0.5,
+             # color = "gray",
+             lty = 2) +
+  geom_line(data = wp_sum_plot2,
+            aes(x = days_since_pulse,
+                y = WP_m,
+                color = pulse_num2)) +
+  geom_errorbar(data = wp_sum_plot2,
+                aes(x = days_since_pulse,
+                    ymin = WP_m - WP_sd,
+                    ymax = WP_m + WP_sd,
+                    color = pulse_num2), 
+                alpha = 0.5, width = 0.15) +
+  geom_point(data = wp_sum_plot2,
+             aes(x = days_since_pulse,
+                 y = WP_m,
+                 color = pulse_num2),
+             size = 1.5) +
+  geom_line(data = vwc_cde |>  filter(depth == "0-12 cm", trt_s != "S4"),
+            aes(x = days_since_pulse,
+                y = mean*25 - 6,
+                color = pulse_num2),
+            linetype = 1) +
+  facet_grid(cols = vars(trt_s),
+             rows = vars(period),
+             scales = "free_x",
+             space = "free_x",
+             switch = "y") +
+  scale_y_continuous(expression(paste(Psi[leaf], " (MPa)")),
+                     sec.axis = sec_axis(~(.+6)/25,
+                                         name = expression(paste(Theta[0-12], " (", cm^3, cm^-3, ")"))) ) +
+  scale_x_continuous("Day of pulse",
+                     breaks = unique(wp_sum_plot2$days_since_pulse)) +
+  scale_color_manual("Pulse",
+                     values = c("coral","cornflowerblue")) +
+  scale_linetype_manual("Variable", values = c(1, 3)) +
+  theme_bw(base_size = 12) +
+  theme(panel.grid = element_blank(),
+        strip.background = element_blank(),
+        strip.placement = "outside",
+        legend.title = element_blank(),
+        legend.position = c(0.875, 0.65),
+        legend.text = element_text(size = 8),
+        legend.background = element_rect(fill = NA)) +
+  guides(color = guide_legend(override.aes = list(linetype = c(1, 1)),
+                              keyheight = 0.75))
+
+##### LWP timeseries by plot for S4 #####
+
+wp_ts_plot <- wp_wide |>
+  # reclassifly pulses as 1 or 2
+  mutate(pulse_num2 = case_when(pulse_num %in% c(3, 7, 13) ~ "Aug 14",
+                                pulse_num %in% c(8, 15) ~ "Aug 21") |> 
+           as.factor(),
+         plot2 = paste0("H", house, "P", plot)) |> 
+  left_join(vpd_plot, by = join_by("date_col" == "date"))
+
+
+ggplot() +
+  geom_point(data = wp_ts_plot,
+             aes(x = date_col, y = predawn,
+                 color = "predawn")) +
+  geom_line(data = wp_ts_plot,
+             aes(x = date_col, y = predawn,
+                 color = "predawn")) +
+  geom_point(data = wp_ts_plot,
+             aes(x = date_col, y = midday,
+                 color = "midday")) +
+  geom_line(data = wp_ts_plot,
+            aes(x = date_col, y = midday,
+                color = "midday")) +
+  facet_wrap(vars(trt_s, plot2), nrow = 3) 
+
+#### RWC timeseries by plot ####
+wp_rwc_plot <- wp_rwc |>
+  filter(trt_s %in% c("S1", "S2", "S4")) |>  
+  # reclassifly pulses as 1 or 2
+  mutate(pulse_num2 = case_when(pulse_num %in% c(3, 7, 13) ~ "Aug 14",
+                                pulse_num %in% c(8, 15) ~ "Aug 21") |> 
+           as.factor(),
+         plot2 = paste0("H", house, "P", plot)) |> 
+  left_join(vpd_plot, by = join_by("date_col" == "date"))
+
+ggplot() +
+  geom_point(data = wp_rwc_plot,
+             aes(x = date_col, y = RWC,
+                 color = period)) +
+  geom_line(data = wp_rwc_plot,
+            aes(x = date_col, y = RWC,
+                color = period)) +
+  facet_wrap(vars(trt_s, plot2), nrow = 3) 
