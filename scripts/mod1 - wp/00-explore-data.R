a96595248de930c699f1bf55070794883c4e0aeb
Jessica Guo <jessicaguo@email.arizona.edu>
2024-03-07 12:33:34 -0700

    Revamp analysis of VPD and SWP as drivers of LWP

--- scripts/mod1 - wp/00-explore-data.R
+++ scripts/mod1 - wp/00-explore-data.R
@@ -1,15 +1,11 @@
 # Bivariate plots to prep for model
+# Revamped for WP only, newly cleaned data
+
 library(tidyverse)
 
 # read in  cleaned datasets
-wp_wide <- read_csv("data_clean/wp_wide.csv")
-# daytime vpd
-vpd_day <- read_csv("data_clean/vpd_daily_daytime.csv") |> 
-  rename("interval" = "period") |> 
-  filter(interval == "day", 
-         location == "inside") |> 
-  select(-location, -interval, -sd) |> 
-  rename("Dmean_day" = mean)
+wp <- read_csv("data_clean/wp_rwc_long.csv") |> 
+  filter(variable == "WP")
 
 # PD and MD VPD
 vpd_int <- read_csv("data_clean/vpd_daily_daytime.csv") |> 
@@ -21,154 +17,100 @@ vpd_int <- read_csv("data_clean/vpd_daily_daytime.csv") |>
    pivot_wider(names_from = interval,
                values_from = mean) |> 
   rename("Dmean_PD" = "PD", "Dmean_MD" = "MD")
-# use morning VWC, between 4:30 am and 12 pm
-vwc <- read_csv("data_clean/vwc_daily_daytime.csv") |> 
+
+# use morning VWCP, between 4:30 am and 12 pm
+swp <- read_csv("data_clean/swp_daily_daytime.csv") |> 
   rename("interval" = "period") |> 
   filter(interval == "morn") |> 
-  mutate(depth2 = case_when(depth == "0-12 cm" ~ "SWC_1",
-                            depth == "25 cm" ~ "SWC_2",
-                            depth == "75 cm" ~ "SWC_3")) |> 
+  mutate(depth2 = case_when(depth == "0-12 cm" ~ "SWP_1",
+                            depth == "25 cm" ~ "SWP_2",
+                            depth == "75 cm" ~ "SWP_3")) |> 
   select(-sd, -depth, -interval) |> 
   pivot_wider(names_from = depth2,
               values_from = mean)
+
 irig <- read_csv("data_clean/irig_long.csv") |> 
-  filter(date >= min(wp_wide$date_col),
-         date <= max(wp_wide$date_col))
+  filter(date >= min(wp$date_col),
+         date <= max(wp$date_col))
+
 cde <- read_csv("data_clean/cde_daytime_pulse.csv") |> 
   select(date, trt_s, cde)
 
 # join together for plotting
- wp_all <- wp_wide |> 
-   left_join(vpd_day, by = join_by("date_col" == "date")) |> 
+ wp_all <- wp |> 
    left_join(vpd_int, by = join_by("date_col" == "date")) |> 
-   left_join(vwc, by = join_by("date_col" == "date", "trt_s" == "summer")) |> 
+   left_join(swp, by = join_by("date_col" == "date", "trt_s" == "summer")) |> 
    # left_join(irig, by = join_by("date_col" == "date", "trt_s")) |> 
-   left_join(cde, by = join_by("date_col" == "date", "trt_s"))  |> 
+   # left_join(cde, by = join_by("date_col" == "date", "trt_s"))  |> 
    mutate(pulse_num2 = case_when(pulse_num %in% c(3, 7, 13) ~ "Aug 14",
                                  pulse_num %in% c(8, 15) ~ "Aug 21") |> 
-            as.factor())
+            as.factor(),
+          period = factor(period, levels = c("predawn", "midday")),
+          Dmean = case_when(period == "predawn" ~ Dmean_PD,
+                            period == "midday" ~ Dmean_MD))
 
  
 # Quick scatterplots
-# can't really use CDE, as it indexes time calculated by pulse and is not generally available
+
+# Check timeseries
 wp_all |> 
   ggplot() +
-  geom_point(aes(x = date_col, y = predawn)) +
-  geom_line(aes(x = date_col, y = SWC_2*10, col = "VWC 25cm")) +
+  geom_point(aes(x = date_col, y = value)) +
+  geom_line(aes(x = date_col, y = SWP_1, col = "SWP 0-10 cm")) +
   facet_wrap(~trt_s)
  
- wp_all |> 
+ 
+# WP vs. Dmean by period, day zero annotated
+wp_all |> 
    ggplot() +
-   geom_point(aes(x = Dmean_PD, y = predawn, col = factor(house))) +
+   geom_point(aes(x = Dmean, y = value, col = period)) +
+   geom_point(data = wp_all |> filter(days_since_pulse == 0),
+              aes(x = Dmean, y = value, col = period),
+              shape = 8) +
    facet_grid(cols = vars(trt_s))
 
+# WP vs. Dmean by period, colored by SWP_1
 wp_all |> 
   ggplot() +
-  geom_point(aes(x = Dmean_MD, y = midday, col = factor(days_since_pulse))) +
-  facet_grid(rows = vars(pulse_num2),
-             cols = vars(trt_s))
- 
- 
+  geom_point(aes(x = Dmean, y = value, col = SWP_1, shape = period)) +
+  geom_point(data = wp_all |> filter(days_since_pulse == 0),
+             aes(x = Dmean, y = value, col = SWP_1),
+             shape = 8) 
+  # facet_grid(# cols = vars(trt_s),
+             # rows = vars(period))
+
+# Regardless of treatment, WP vs. SWP shallow by period
  wp_all |> 
    ggplot() +
-   geom_point(aes(x = SWC_2, y = midday, col = factor(days_since_pulse))) +
-   facet_wrap(~trt_s)
+   geom_point(aes(x = SWP_1, y = value, col = trt_s)) +
+   geom_point(data = wp_all |> filter(days_since_pulse == 0),
+              aes(x = SWP_1, y = value, col = trt_s),
+              shape = 8) +
+   facet_wrap(~period, ncol = 1)
  
+ # Test interaction of SWP and VPD
+ # WP vs. Dmean by period, color is VPD
+ wp_all |> 
+   filter(period == "predawn") |> 
+   ggplot() +
+   geom_point(aes(x = SWP_1, y = value, col = Dmean)) +
+   geom_point(data = wp_all |> filter(days_since_pulse == 0,
+                                      period == "predawn"),
+              aes(x = SWP_1, y = value, col = Dmean),
+              shape = 8)
+ 
+ # Regardless of treatment, WP vs. SWP mid by period
  wp_all |> 
    ggplot() +
-   geom_point(aes(x = SWC_3, y = midday, col = factor(days_since_pulse))) +
-   facet_grid(rows = vars(pulse_num2),
-              cols = vars(trt_s))
+   geom_point(aes(x = SWP_2, y = value, col = trt_s)) +
+   geom_point(data = wp_all |> filter(days_since_pulse == 0),
+              aes(x = SWP_2, y = value, col = trt_s),
+              shape = 8) +
+   facet_wrap(~period, ncol = 1)
+# No standard relationship, because highly variable SWP_2 by treatment
+# Only S4 seems to have a standard relationship
  
- ### Check VPD options
- # daytime for now, consider using PD and MD
- vpd_all <- read_csv("data_clean/vpd_daily_daytime.csv") |> 
-   rename("interval" = "period") |> 
-   filter(location == "inside") |> 
-   select(-location, -sd) |> 
-   rename("Dmean" = mean)
  
- vpd_all |> 
-   ggplot(aes(x = date, y = Dmean,
-              color = interval)) +
-   geom_point() +
-   geom_line() +
-   theme_bw()
-
- vpd_wide <- vpd_all |> 
-   filter(interval %in% c("PD", "MD"),
-          !is.na(Dmean)) |> 
-   pivot_wider(names_from = interval,
-               values_from = Dmean)
-cor(vpd_wide$PD, vpd_wide$MD) 
-cor(wp_wide$predawn, wp_wide$midday)
-cor(wp_all$SWC_1, wp_all$SWC_2)
-cor(wp_all$SWC_2, wp_all$SWC_3)
-cor(wp_all$SWC_1, wp_all$SWC_3)
-
-wp_long <- wp_wide |> 
-  rename("PD" = "predawn", "MD" = "midday") |> 
-  pivot_longer(cols = c("PD", "MD"),
-               names_to = "period",
-               values_to = "WP") |> 
-  left_join(vpd_all |> filter(interval %in% c("PD", "MD")),
-            by = join_by("date_col" == "date",
-                         "period" == "interval")) |> 
-  rename("Dmean_period" = "Dmean") |> 
-  left_join(vpd_day, by = join_by("date_col" == "date")) |> 
-  left_join(vwc, by = join_by("date_col" == "date", "trt_s" == "summer")) |> 
-  mutate(pulse_num2 = case_when(pulse_num %in% c(3, 7, 13) ~ "Aug 14",
-                                pulse_num %in% c(8, 15) ~ "Aug 21") |> 
-           as.factor())
-
-wp_long |> 
-  ggplot(aes(x = Dmean_day, y = WP, col = period)) +
-  geom_point() +
-  facet_wrap(~trt_s)
-
-wp_long |> 
-  ggplot(aes(x = Dmean_period, y = WP, col = period)) +
-  geom_point(aes(shape = factor(house))) +
-  facet_grid(cols = vars(trt_s))
-
-wp_long |> 
-  ggplot(aes(x = SWC_1, y = WP, col = period)) +
-  geom_point(aes(shape = factor(house))) +
-  facet_grid(cols = vars(trt_s))
-
-####  Across all treatments, thresholds in SWC and SWP for WP? ####
-wp_long |> 
-  ggplot(aes(x = SWC_1, y = WP, col = trt_s)) +
-  geom_point(aes(shape = factor(house))) +
-  facet_wrap(~period) +
-  theme_bw()
-
-wp_long |> 
-  filter(pulse_num2 == "Aug 14") |>
-  # filter(period == "PD") |> 
-  ggplot(aes(x = SWC_1, y = WP, col = trt_s)) +
-  geom_point(aes(shape = factor(house))) +
-  geom_path(arrow = arrow(type = "closed",
-                          length = unit(0.2, "cm")),
-            show.legend = FALSE,
-            color = "black") +
-  facet_grid(cols = vars(trt_s),
-             rows = vars(period)) +
-  theme_bw()
-
-# check for house REs
-wp_long |> 
-  group_by(date_col, house, period) |> 
-  summarize(WP_m = mean(WP),
-            WP_sd = sd(WP)) |> 
-  ggplot(aes(x = date_col, col = factor(house), shape = period)) +
-  geom_point(aes(y = WP_m)) +
-  geom_line(aes(y = WP_m)) +
-  geom_errorbar(aes(ymin = WP_m - WP_sd,
-                    ymax = WP_m + WP_sd),
-                width = 0)
-
-
-### Save out
-write_csv(wp_wide, "scripts/mod1 - pd/data_wide.csv")
-write_csv(wp_long, "scripts/mod1 - pd/data_long.csv")
+# Write out joined data
+ save(wp_all, file = "scripts/mod1 - wp/wp_all.Rdata")
+ 
--- scripts/mod1 - wp/01-run-model.R
+++ scripts/mod1 - wp/01-run-model.R
@@ -6,23 +6,19 @@ load.module("dic")
 library(mcmcplots)
 library(broom.mixed)
 
-dat_df <- read_csv("scripts/mod1 - wp/data_long.csv")
+load(file = "scripts/mod1 - wp/wp_all.Rdata")
 
-dat_df |> 
-  group_by(date_col, house, period) |> 
-  summarize(WP_m = mean(WP)) |> 
-  group_by(date_col, period) |> 
-  summarize(WP_sd = sd(WP_m))
+str(wp_all)
 
-datlist <- list(wp = dat_df$WP,
-                D = as.vector(scale(dat_df$Dmean_period)), # PD or MD scaled VPD
-                W1 = as.vector(scale(dat_df$SWC_1)), # morning only 4:30 am to 12 noon
-                W2 = as.vector(scale(dat_df$SWC_2)), # morning only 4:30 am to 12 noon
-                house = dat_df$house,
-                N = nrow(dat_df),
-                Nparam = 7,
-                Nhouse = length(unique(dat_df$house)),
+datlist <- list(wp = wp_all$value,
+                D = as.vector(scale(wp_all$Dmean)), # matched to predawn or midday
+                SWP1 = as.vector(scale(wp_all$SWP_1)), # morning only 4:30 am to 12 noon
+                ID = factor(wp_all$ID),
+                N = nrow(wp_all),
+                Nparam = 4,
+                Nid = length(unique(wp_all$ID)),
                 Sa = 1)
+str(datlist)
 
 # Function to generate initials
 init <- function() {
@@ -33,17 +29,20 @@ init <- function() {
 
 initslist <- list(init(), init(), init())
 
+str(initslist)
 # Initialize model
 jm <- jags.model("scripts/mod1 - wp/mod1-simple.JAGS",
                  data = datlist,
                  inits = initslist,
                  n.chains = 3)
 
-update(jm, n.iter = 10000)
+update(jm, n.iter = 20000)
+
+dic.samples(jm, n.iter = 10000)
 
 
 # Monitor
-params <- c("deviance", "Dsum",
+params <- c("deviance", "Dsum", "R2",
             "B", "Bstar",
             "tau", "sig", 
             "sig.eps.a", "sig.eps", 
@@ -51,11 +50,11 @@ params <- c("deviance", "Dsum",
 )
 coda.out <- coda.samples(jm,
                          variable.names = params,
-                         n.iter = 3000,
+                         n.iter = 15000,
                          n.thin = 15)
 
 # Inspect chains visually
-mcmcplot(coda.out, parms = c("deviance", "Dsum", 
+mcmcplot(coda.out, parms = c("deviance", "Dsum", "R2",
                              "Bstar", "Estar",
                              "sig", "sig.eps"))
 
@@ -67,12 +66,12 @@ caterplot(coda.out, parms = "Estar", reorder = FALSE)
 
 coda.rep <- coda.samples(jm, 
                          variable.names = c("wp.rep"),
-                         n.iter = 3000,
+                         n.iter = 15000,
                          n.thin = 15)
 
 
 # Summarize replicated output
-coda_sum <- tidyMCMC(coda.rep,
+rep_sum <- tidyMCMC(coda.rep,
                      conf.int = TRUE,
                      conf.method = "HPDinterval") %>%
   rename(pred.mean = estimate,
@@ -81,18 +80,17 @@ coda_sum <- tidyMCMC(coda.rep,
 
 
 # Check model fit
-pred <- cbind.data.frame(dat_df, coda_sum)
+pred <- cbind.data.frame(wp_all, rep_sum)
 
-m1 <- lm(pred.mean ~ WP, data = pred)
-summary(m1) # R2 = 0.6847
+m1 <- lm(pred.mean ~ value, data = pred)
+summary(m1) # R2 = 0.7832
 
 pred %>%
-  ggplot(aes(x = WP, y = pred.mean, col = period)) +
+  ggplot(aes(x = value, y = pred.mean, col = period)) +
   geom_abline(intercept = 0, slope = 1, col = "black", lty = 2) +
   geom_errorbar(aes(ymin = pred.lower, ymax = pred.upper),
                 alpha = 0.25) +
   geom_point() +
   scale_x_continuous("Observed") +
   scale_y_continuous("Predicted") +
-  theme_bw(base_size = 12) +
-  facet_wrap(~trt_s)
+  theme_bw(base_size = 12)
