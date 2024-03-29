3c7d42333d10196d9b56e160566454953d71196b
Jessica Guo <jessicaguo@email.arizona.edu>
2024-03-29 16:19:34 -0700

    Try with lme4::lmer() and use the Kenward-Roger approximation for df to conduct t-tests

--- scripts/10-model-lwp-phase.R
+++ scripts/10-model-lwp-phase.R
@@ -5,6 +5,8 @@ library(tidyverse)
 library(emmeans)
 library(broom)
 library(nlme)
+library(lme4)
+library(pbkrtest)
 
 # Load S1, S2, and S4 LWP data
 wp <- read_csv("data_clean/wp_rwc_long.csv") |> 
@@ -92,6 +94,8 @@ pairs(EMM2)
 mm1 <- lme(value ~ period2 + Dmean*phase, random= ~1|ID, data = wp_all)
 summary(mm1)
 coef(mm1)
+plot(ranef(mm1)) # 16 total plots, random intercepts
+plot(residuals(mm1))
 anova(mm1) # interaction between Dmean:phase is not significant
 glance(mm1)
 # Only use significant parameters
@@ -107,3 +111,79 @@ glance(mm2)
 # Only use significant parameters
 tm2 <- broom::tidy(mm2) |>
   filter(p.value < 0.05)
+
+
+# Test with Kenward-Roger approximation for degrees of freedom
+# Requires using lme4
+
+# Does the effect of VPD change by phase, depending on time of day?
+mr1 <- lme4::lmer(value ~ period2 + Dmean*phase + (1|ID), data = wp_all)
+summary(mr1)
+vcovadj <- vcovAdj(mr1)
+alpha <- 0.05
+digits <- 3
+pval.digits <- 3
+p.val.ub <- 0.001
+
+model.fixed.effects1 <- lme4::fixef(mr1)
+fixed.effects.contrasts <- diag(length(model.fixed.effects))
+model.fixef.results1 <- data.frame(parameter = as.character(rep(NA, length(model.fixed.effects))),
+                                   estimate = as.numeric(rep(NA, length(model.fixed.effects))),
+                                   ci = as.character(rep(NA, length(model.fixed.effects))),
+                                   den.df = as.numeric(rep(NA, length(model.fixed.effects))),
+                                   tstat = as.numeric(rep(NA, length(model.fixed.effects))),
+                                   pval = as.character(rep(NA, length(model.fixed.effects))))
+# Do linear hypothesis tests to isolate the effect of each parameter:
+for (r in 1:length(model.fixed.effects1)) {
+  contrast.mat <- matrix(fixed.effects.contrasts[r, ], nrow = 1)
+  df <- get_Lb_ddf(mr1, contrast.mat)
+  pt.est <- fixef(mr1) %*% t(contrast.mat)
+  vcov.est <- contrast.mat %*% vcovadj %*% t(contrast.mat)
+  sd.est <- sqrt(vcov.est)
+  t.stat <- sqrt(as.numeric(pt.est %*% solve(vcov.est) %*% t(pt.est)))
+  t.stat <- ifelse(pt.est < 0, -1*t.stat, t.stat)
+  p.val <- 2*pt(abs(t.stat), df, lower.tail = F)
+  model.fixef.results1[r, c("parameter", "ci", "pval")] <- c(names(model.fixed.effects1)[r],
+                                                            paste0("(", round(pt.est - sd.est*qt(1 - alpha/2, df, lower.tail = T), digits),
+                                                                      ", ", round(pt.est + sd.est*qt(1 - alpha/2, df, lower.tail = T), digits), ")"),
+                                                            ifelse(round(p.val, pval.digits) == 0, p.val.ub, round(p.val, pval.digits)))
+  model.fixef.results1[r, c("estimate", "den.df", "tstat")] <- round(c(pt.est, df, t.stat), digits)
+}
+
+write_csv(model.fixef.results1, "tables/VPD_phase_KR.csv")
+
+# Does the effect of SWP change by phase, depending on time of day?
+mr2 <- lme4::lmer(value ~ period2 + SWP_1*phase + (1|ID), data = wp_all)
+summary(mr2)
+vcovadj <- vcovAdj(mr2)
+alpha <- 0.05
+digits <- 3
+pval.digits <- 3
+p.val.ub <- 0.001
+
+model.fixed.effects2 <- lme4::fixef(mr2)
+fixed.effects.contrasts <- diag(length(model.fixed.effects2))
+model.fixef.results2 <- data.frame(parameter = as.character(rep(NA, length(model.fixed.effects))),
+                                   estimate = as.numeric(rep(NA, length(model.fixed.effects))),
+                                   ci = as.character(rep(NA, length(model.fixed.effects))),
+                                   den.df = as.numeric(rep(NA, length(model.fixed.effects))),
+                                   tstat = as.numeric(rep(NA, length(model.fixed.effects))),
+                                   pval = as.character(rep(NA, length(model.fixed.effects))))
+# Do linear hypothesis tests to isolate the effect of each parameter:
+for (r in 1:length(model.fixed.effects2)) {
+  contrast.mat <- matrix(fixed.effects.contrasts[r, ], nrow = 1)
+  df <- get_Lb_ddf(mr2, contrast.mat)
+  pt.est <- fixef(mr2) %*% t(contrast.mat)
+  vcov.est <- contrast.mat %*% vcovadj %*% t(contrast.mat)
+  sd.est <- sqrt(vcov.est)
+  t.stat <- sqrt(as.numeric(pt.est %*% solve(vcov.est) %*% t(pt.est)))
+  t.stat <- ifelse(pt.est < 0, -1*t.stat, t.stat)
+  p.val <- 2*pt(abs(t.stat), df, lower.tail = F)
+  model.fixef.results2[r, c("parameter", "ci", "pval")] <- c(names(model.fixed.effects2)[r],
+                                                            paste0("(", round(pt.est - sd.est*qt(1 - alpha/2, df, lower.tail = T), digits),
+                                                                   ", ", round(pt.est + sd.est*qt(1 - alpha/2, df, lower.tail = T), digits), ")"),
+                                                            ifelse(round(p.val, pval.digits) == 0, p.val.ub, round(p.val, pval.digits)))
+  model.fixef.results2[r, c("estimate", "den.df", "tstat")] <- round(c(pt.est, df, t.stat), digits)
+}
+
+write_csv(model.fixef.results2, "tables/SWP_phase_KR.csv")
