a73a5ed1d1054a6d6bfc65d2efa1aeda5832157a
Jessica Guo <jessicaguo@email.arizona.edu>
2024-05-27 09:28:12 -0700

    Poster version

---
+++ fig_scripts/gordon_poster/fig2.R
@@ -0,0 +1,161 @@
+# 2 or 3 panel plot with
+# a) moisture release curves by depth (WP4 data + Gardner models)
+# b) double axis timeseries SWP and VWC by depth, for whole pulse period
+# boxes to highlight pulse chase duration?
+# c or supplemental) comparison of predicted and measured SWP for house with data in 2023 pulse chase
+
+library(tidyverse)
+source("source/vwc2swp.R")
+library(RColorBrewer)
+library(cowplot)
+
+#### Fig A: moisture release curves by depth ####
+
+# Read in raw WP4 data
+wp4 <- read_csv("data_clean/moisture_release.csv") |> 
+  mutate(Depth = case_when(depth == 10 ~ "10 cm",
+                           depth == 25 ~ "25 cm"))
+
+# Create predicted data
+mrc <- data.frame(vwc = seq(0.015, 0.092, by = 0.0005)) |> 
+  mutate(swp_10 = vwc2swp(vwc, param = "depth10cm"),
+         swp_25 = vwc2swp(vwc, param = "depth25cm")) |> 
+  pivot_longer(starts_with("swp"),
+               names_to = "depth",
+               values_to = "swp") |> 
+  mutate(Depth = case_when(depth == "swp_10" ~ "10 cm",
+                           depth == "swp_25" ~ "25 cm"))
+
+# colors
+cols_pur <-brewer.pal(7, "PRGn")
+display.brewer.pal(7, "PRGn")
+
+fig_a <- wp4 |> 
+  filter(vwc > 0.015) |> # limit for lookup table
+  ggplot() +
+  geom_point(aes(x = vwc, y = -mpa, color = Depth)) +
+  geom_line(data = mrc,
+            aes(x = vwc, y = -swp, color = Depth)) +
+  scale_x_continuous(expression(paste(Theta, " (", cm^3, " ", cm^-3, ")"))) +
+  scale_y_continuous(expression(paste(Psi[soil], " (-MPa)"))) +
+  scale_color_manual(values = cols_pur[2:1]) +
+  theme_bw(base_size = 14) +
+  theme(panel.grid = element_blank(),
+        legend.position = c(0.75, 0.85),
+        legend.title = element_blank(),
+        legend.text = element_text(size = 12),
+        legend.background = element_blank())
+
+ggsave("fig_scripts/gordon_poster/fig2a.png",
+       fig_a,
+       height = 3,
+       width = 3,
+       units = "in")
+
+
+# Load instrument/predicted timeseries for study period
+swp <- read_csv("data_clean/swp_daily_daytime.csv") |> 
+  filter(period == "morn",
+         depth == "0-12 cm",
+         summer != "S3",
+         date >= as.Date("2023-08-14", tz = "America/Phoenix"),
+         date <= as.Date("2023-09-04", tz = "America/Phoenix")) |>
+  mutate(trt_label = case_when(summer == "S1" ~ "P3.5",
+                               summer == "S2" ~ "P7",
+                               summer == "S4" ~ "P21") |>
+           factor(levels = c("P3.5", "P7", "P21")))
+
+vwc <- read_csv("data_clean/vwc_daily_daytime.csv") |> 
+  filter(period == "morn",
+         depth == "0-12 cm",
+         summer != "S3",
+         date >= as.Date("2023-08-14", tz = "America/Phoenix"),
+         date <= as.Date("2023-09-04", tz = "America/Phoenix")) |>
+  mutate(trt_label = case_when(summer == "S1" ~ "P3.5",
+                               summer == "S2" ~ "P7",
+                               summer == "S4" ~ "P21") |>
+           factor(levels = c("P3.5", "P7", "P21")))
+
+# Create shading boxes for each pulse
+
+pulse <- data.frame(summer = c("S4", "S1", "S2", "S1", "S2"),
+                    trt_label = c("P21", "P3.5", "P7", "P3.5", "P7"),
+                    pulse_num = c(1, 1, 1, 2, 2),
+                    st = c(as.Date("2023-08-14", tz = "America/Phoenix"),
+                           as.Date("2023-08-14", tz = "America/Phoenix"),
+                           as.Date("2023-08-14", tz = "America/Phoenix"),
+                           as.Date("2023-08-21", tz = "America/Phoenix"),
+                           as.Date("2023-08-21", tz = "America/Phoenix")),
+                    en = c(as.Date("2023-09-04", tz = "America/Phoenix"),
+                           as.Date("2023-08-17", tz = "America/Phoenix"),
+                           as.Date("2023-08-21", tz = "America/Phoenix"),
+                           as.Date("2023-08-24", tz = "America/Phoenix"),
+                           as.Date("2023-08-28", tz = "America/Phoenix"))) |>
+  mutate(trt_label = factor(trt_label, levels = c("P3.5", "P7", "P21")))
+
+
+# colors
+cols_br_gn <- brewer.pal(7, "BrBG")
+display.brewer.pal(7, "BrBG")
+
+# labels
+labs <- c(lapply(c("soil"), function(i) bquote(Psi[.(i)])),
+          bquote(Theta))
+
+
+fig_b <- ggplot() +
+  geom_rect(data = pulse,
+            aes(xmin = st, xmax = en,
+                ymin = -Inf, ymax = Inf),
+            alpha = 0.1) +
+  geom_line(data = vwc,
+            aes(x = date, y = mean,
+                color = "VWC",
+                lty = depth),
+            linewidth = 1.5) +
+  geom_line(data = swp,
+            aes(x = date, y = (mean + 2.7)/20, # 15 and 2 also work well, but show negative vwc's
+                color = "SWP",
+                lty = depth),
+            linewidth = 1.5) +
+  scale_y_continuous(expression(paste(Theta, " (", cm^3, " ", cm^-3, ")")),
+                     sec.axis = sec_axis(~.*20-2.7,
+                                         expression(paste(Psi[soil], " (MPa)")))) +
+  scale_x_date(breaks = as.Date(c("2023-08-14", "2023-08-21", 
+                                  "2023-08-28")),
+               date_labels = "%b %d") +
+  scale_color_manual(values = cols_br_gn[1:2],
+                     label = labs) +
+  scale_linetype_manual(values = c("solid", "longdash")) +
+  facet_grid(cols = vars(trt_label),
+             # rows = vars(depth),
+             scales = "free_y",
+             space = "free_y") +
+  guides(linetype = "none") +
+  theme_bw(base_size = 18) +
+  theme(panel.grid = element_blank(),
+        # strip.placement.x = "outside",
+        legend.title = element_blank(),
+        legend.position = c(0.25, 0.9),
+        legend.background = element_blank(),
+        legend.text = element_text(size = 16),
+        legend.key.size = unit(0.35, "cm"),
+        strip.background.x = element_blank(),
+        strip.background.y = element_rect(fill = "transparent"),
+        axis.title.x = element_blank(),
+        axis.title.y.left = element_text(color = cols_br_gn[2]),
+        axis.title.y.right = element_text(color = cols_br_gn[1]))
+
+
+#### Combine and print out ####
+
+# fig2 <- plot_grid(fig_a, fig_b, ncol = 2,
+#           rel_widths = c(1, 3),
+#           align = "h", axis = "bt",
+#           labels = "auto")
+
+ggsave("fig_scripts/gordon_poster/fig2.png",
+       fig_b,
+       height = 4,
+       width = 10,
+       units = "in")
---
+++ fig_scripts/gordon_poster/fig4.R
@@ -0,0 +1,217 @@
+# Changepoint model with parameters for S4 PD only
+# Plot with SWP and VPD
+# Derives the phases 0/1/2 for further analysis
+
+library(coda)
+library(tidyverse)
+library(broom.mixed)
+library(ggh4x)
+library(RColorBrewer)
+
+
+# Load data
+load("scripts/mod4 - piecewise/s4_all.Rdata")
+temp <- dat2 |> 
+  filter(variable == "WP",
+         period == "predawn") |> 
+  mutate(plot = factor(ID))
+# n=54 S4 predawns
+
+vpd <- read_csv("data_clean/vpd_daily_daytime.csv") |> 
+  filter(location == "outside",
+         period == "PD", 
+         date >= min(temp$date_col),
+         date <= max(temp$date_col)) |> 
+  rename(Dmean = mean) |> 
+  select(-period, -location, -sd) |> 
+  mutate(days_since_pulse = 0:21)
+
+swp <- read_csv("data_clean/swp_daily_daytime.csv") |> 
+  filter(period == "morn",
+         summer == "S4",
+         depth %in% c("0-12 cm", "25 cm"),
+         date >= min(temp$date_col),
+         date <= max(temp$date_col)) |> 
+  select(-period, -sd, depth) |> 
+  pivot_wider(names_from = depth,
+              values_from = mean) |>
+  rename(trt_s = summer, SWP_1 = `0-12 cm`, SWP_2 = `25 cm`) |> 
+  mutate(days_since_pulse = 0:21)
+
+# Load coda_params
+load("scripts/mod4 - piecewise/coda/coda_params_mod1.Rdata")
+
+param_sum <- tidyMCMC(coda_params,
+                      conf.int = TRUE,
+                      conf.method = "HPDinterval") %>%
+  rename(pred.mean = estimate,
+         pred.lower = conf.low,
+         pred.upper = conf.high)
+
+# Load coda_pred
+load("scripts/mod4 - piecewise/coda/coda_pred_mod1.Rdata")
+
+# Summarize replicated data
+pred_sum <- tidyMCMC(coda_pred,
+                     conf.int = TRUE,
+                     conf.method = "HPDinterval") %>%
+  rename(pred.mean = estimate,
+         pred.lower = conf.low,
+         pred.upper = conf.high)
+
+# Join predicted with original and soil water potential for plotting
+preds <- cbind.data.frame(temp, pred_sum) 
+
+# Obtain other parameters of interest
+cps <- param_sum |> 
+  filter(grepl("^mu\\.cp", term))
+
+maxy <- param_sum |> 
+  filter(grepl("mu\\.maxy", term))
+
+ab1 <- param_sum |> 
+  filter(grepl("mu\\.a$", term) | grepl("mu\\.b\\[1", term)) |> 
+  mutate(var = c("intercept", "slope"))
+
+ab2 <- param_sum |> 
+  filter(grepl("mu\\.a2", term) | grepl("mu\\.b\\[2", term)) |> 
+  mutate(var = c("intercept", "slope"))
+
+#  Establish colors
+cols_br_gn <- brewer.pal(7, "BrBG")
+display.brewer.pal(7, "BrBG")
+cols_gn <- brewer.pal(4, "Paired")
+display.brewer.pal(4, "Paired")
+cols_div <- brewer.pal(7, "Spectral")
+display.brewer.pal(7, "Spectral")
+
+# Create labels 
+labs <- c(lapply(c("PD", "0-12 cm"), function(i) bquote(Psi[.(i)])),
+          lapply(c("PD"), function(i) bquote(VPD[.(i)])))
+
+
+# Create dataframe for Phase labels
+phases <- data.frame(lab = c("", "Phase 1", "Phase 2"),
+                     xmin = c(-Inf, cps$pred.mean),
+                     xmax = c(cps$pred.mean, Inf),
+                     ymin = rep(0, 3),
+                     ymax = rep(Inf, 3),
+                     xmean = c(0, mean(cps$pred.mean), 
+                               mean(c(cps$pred.mean[2], 21))),
+                     ymean = rep(0.5, 3))
+
+fig4 <-  preds |> 
+  ggplot() +
+  geom_line(data = swp, 
+            aes(x = days_since_pulse, y = SWP_1,
+                color = "SWP_1"),
+            lty = "solid",
+            linewidth = 1.25) +
+  # geom_line(data = swp, 
+  #           aes(x = days_since_pulse, y = SWP_2,
+  #               color = "SWP_2"),
+  #           lty = "longdash") +
+  geom_line(data = vpd, 
+            aes(x = days_since_pulse, y = Dmean-4.5,
+                color = "VPD"),
+            lty = "solid",
+            linewidth = 1.25) +
+  geom_hline(data = maxy, 
+             aes(yintercept = pred.mean),
+             linetype = "dotted") +
+  geom_rect(data = cps,
+            aes(ymin = -Inf, ymax = Inf,
+                xmin = pred.lower, xmax = pred.upper),
+            color = "gray90", alpha = 0.15) +
+  geom_vline(data = cps,
+             aes(xintercept = pred.mean)) +
+  geom_abline(slope = ab1$pred.mean[2],
+              intercept = ab1$pred.mean[1],
+              linetype = "dotted") +
+  geom_abline(slope = ab2$pred.mean[2],
+              intercept = ab2$pred.mean[1],
+              linetype = "dotted") +
+  geom_point(aes(x = days_since_pulse, y = value,
+                 color = "LWP"),
+             size = 2) +
+  geom_rect(data = phases,
+            aes(xmin = xmin, xmax = xmax,
+                ymin = ymin, ymax = ymax, 
+                fill = lab),
+            color = NA) +
+  geom_text(data = phases,
+            aes(x = xmean, y = ymean,
+                label = lab),
+            size = 4.5,
+            vjust = 1, hjust = 0.5) +
+  scale_y_continuous(expression(paste(Psi[soil], " | ", Psi[plant], " (MPa)")), 
+                     limits = c(-4.5, 0.5),
+                     sec.axis = sec_axis(~.+4.5, 
+                                         "VPD (kPa)",
+                                         breaks = 0:2)) +
+  scale_x_continuous(name = "Days since P21 pulse", 
+                     minor_breaks = seq(0, 21, 1),
+                     breaks = seq(0, 21, 3),
+                     limits = c(0, 21),
+                     guide = "axis_minor") +
+  scale_color_manual(values = c(cols_gn[4], cols_br_gn[1], "coral"),
+                     labels = labs) +
+  scale_fill_manual(values = cols_div[c(3,6,3)]) +
+  theme_bw(base_size = 16) +
+  theme(ggh4x.axis.ticks.length.minor = rel(1),
+        panel.grid = element_blank(),
+        legend.title = element_blank(),
+        legend.text = element_text(size = 14),
+        legend.position = "inside",
+        legend.position.inside = c(0.3, 0.4),
+        legend.background = element_blank(),
+        axis.title.y.right = element_text(color = "coral")) +
+  guides(fill = "none",
+         color = guide_legend(override.aes = list(shape = c(16, NA, NA),
+                                                  linetype = c(NA, 1, 1))))
+
+
+
+ggsave(filename = "fig_scripts/gordon_poster/fig4.png",
+       plot = fig4,
+       height = 3.5,
+       width = 6,
+       units = "in")
+
+#### Calculations ####
+# What range of SWP is encompassed by the shift from
+# Phase 1 to Phase 2?
+
+cp_range <- c(cps[2, 4], cps[2, 5])
+
+swp |> 
+  filter(days_since_pulse >= cp_range[1],
+         days_since_pulse <= cp_range[2]) |> 
+  pull(SWP_1) |> 
+  range() # -1.22 to -0.65
+
+swp |> 
+  filter(days_since_pulse >= cp_range[1],
+         days_since_pulse <= cp_range[2]) |> 
+  pull(SWP_1) |> 
+  mean() # -0.92
+
+# As a rule of thumb, we define the threshold as SWP_1 = -1 MPa
+# Which in this soil is equivalent to VWC ~= 0.04 cm3/cm3
+
+swp |> 
+  filter(days_since_pulse >= cp_range[1],
+         days_since_pulse <= cp_range[2]) |> 
+  pull(SWP_2) |> 
+  range() # -1.22 to -0.65
+
+swp |> 
+  filter(days_since_pulse >= cp_range[1],
+         days_since_pulse <= cp_range[2]) |> 
+  pull(SWP_2) |> 
+  mean() # -0.57
+
+# As a rule of thumb, we define the threshold as SWP_2 = -0.6 MPa
+# Which in this soil is equivalent to VWC ~= 0.0495 cm3/cm3
+
+
