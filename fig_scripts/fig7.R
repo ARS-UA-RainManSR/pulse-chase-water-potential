1c4b19652c6c28bcb9496504845ce5affed44c53
Jessica Guo <jessicaguo@hmc.edu>
2025-09-04 09:33:32 -0700

    Replace gs-swp panels with gs-pd

--- fig_scripts/fig7.R
+++ fig_scripts/fig7.R
@@ -26,6 +26,12 @@ vpd <- read_csv("data_clean/vpd_daily_daytime.csv") |>
   dplyr::select(-location) |> 
   rename(D_morn_mean = mean, D_morn_sd = sd)
 
+
+# Load S1, S2, and S4 LWP data
+wp_pd <- read_csv("data_clean/wp_rwc_long.csv") |> 
+  filter(variable == "WP",
+         period == "predawn")
+
 # Load gs, rerun model, calculate resids
 gst <- read_csv(file = "data_clean/gs_leaftemp.csv",
                 locale = locale(tz = "America/Phoenix")) |>
@@ -39,18 +45,37 @@ gst <- read_csv(file = "data_clean/gs_leaftemp.csv",
          swp_0_10_sd = sd) |> 
   left_join(vpd, join_by(date_col == date)) |> 
   select(-period) |> 
-  mutate(phase = if_else(swp_0_10_mean > -1, "Phase 1", "Phase 2"))
+  mutate(phase = if_else(swp_0_10_mean > -1, "Phase 1", "Phase 2"))|> 
+  left_join(wp_pd, join_by(date_col, trt_s, ID)) |> 
+  rename(predawn = value)
 
-gs.swp.phase <- lme4::lmer(gs ~ phase + swp_0_10_mean*phase + (1|ID), data = gst)
-
-gst <- gst |> 
+# Check high leverage pd values
+outlier <- gst |> 
+  relocate(predawn, .after = gs) |> 
+  filter(phase == "Phase 1") |> 
   ungroup() |> 
-  mutate(resids_gs = residuals(gs.swp.phase))
+  arrange(predawn) |> 
+  slice_head(n = 1)
 
-gst_sum <- gst |>
+# High leverage point occurs on 8/26 in H1P2, S2 treatment
+# Day of minimal difference between predawn and midday (-1.84 vs. -1.91)
+# Even as mean SWP is -0.91. Already disconnected from soil?
+
+gst_remove_outlier <- gst |> 
+  anti_join(outlier)
+
+gs.pd.phase <- lme4::lmer(gs ~ phase + predawn*phase + (1|ID), data = gst_remove_outlier)
+
+gst_remove_outlier <- gst_remove_outlier |> 
+  ungroup() |> 
+  mutate(resids_gs = residuals(gs.pd.phase))
+
+gst_sum <- gst_remove_outlier |>
   group_by(trt_s, date_col) |>
   summarize(gs_m = mean(gs),
-            gs_sd = sd(gs)) |>
+            gs_sd = sd(gs),
+            pd_m = mean(predawn),
+            pd_sd = sd(predawn)) |>
   left_join(swp, by = join_by(trt_s == summer, date_col == date)) |> 
   select(-depth, -period) |> 
   rename(swp_0_10_mean = mean,
@@ -120,12 +145,12 @@ strip <- strip_themed(background_x = elem_list_rect(fill = c(cols_div[c(6,3)])))
 #### gs ####
 
 # Switch to plotable params
-model.fixef.results2 <- read_csv("tables/fluxes/gs_SWP_cellmeans_KR.csv")
+model.fixef.results2 <- read_csv("tables/fluxes/gs_PD_cellmeans_KR.csv")
 
 gs_params <- model.fixef.results2 |>
   separate(parameter, into = c("phase", "term"), sep = ":") |>
   # uncount(c(1,1,2,1,1,2)) |>
-  mutate(type = case_when(term == "SWP" ~ "slope",
+  mutate(type = case_when(term == "PD" ~ "slope",
                           .default = "intercept"),
          sig = ifelse(pval < 0.05, TRUE, FALSE),
          sig = ifelse(type == "slope", sig, NA)) |>
@@ -154,18 +179,23 @@ gs_resid_params <- cell_means |>
 
 # A
 figa <- ggplot() +
-  geom_point(data = gst,
-             aes(x = swp_0_10_mean,
+  geom_point(data = gst_remove_outlier,
+             aes(x = predawn,
                  y = gs),
              size = 1.5,
              alpha = 0.25) +
+  geom_errorbarh(data = gst_sum, 
+                aes(y = gs_m,
+                    xmin = pd_m - pd_sd,
+                    xmax = pd_m + pd_sd),
+                alpha = 0.5) +
   geom_errorbar(data = gst_sum, 
-                aes(x = swp_0_10_mean,
+                aes(x = pd_m,
                     ymin = gs_m - gs_sd,
                     ymax = gs_m + gs_sd),
                 alpha = 0.5) +
   geom_point(data = gst_sum,
-              aes(x = swp_0_10_mean,
+              aes(x = pd_m,
                  y = gs_m),
              size = 3) +
   geom_abline(data = gs_params,
@@ -173,12 +203,13 @@ figa <- ggplot() +
                   intercept = intercept,
                   lty = sig)) +
   scale_y_continuous(expression(paste(g[s], " (mmol ", H[2], "O ", m^-2, " ", s^-1, ")"))) +
-  scale_x_reverse(expression(paste(Psi[soil], " (MPa)"))) +
-  scale_linetype_manual(values = "solid") +
-  facet_wrap2(~phase,
+  scale_x_reverse(expression(paste(Psi[PD], " (MPa)"))) +
+  scale_linetype_manual(values = c("longdash", "solid")) +
+  facet_grid2(~phase,
               strip = strip,
               # space = "free_x",
               scales = "free_x") +
+  force_panelsizes(cols = c(1,1.75)) +
   theme_bw(base_size = 12) +
   theme(panel.grid = element_blank(),
         strip.background = element_blank(),
@@ -187,9 +218,10 @@ figa <- ggplot() +
   guides(shape = "none",
          lty = "none")
 
+figa
 # B
 figb <- ggplot() +
-  geom_point(data = gst,
+  geom_point(data = gst_remove_outlier,
              aes(x = D_morn_mean,
                  y = resids_gs,
                  color = phase),
@@ -204,11 +236,24 @@ figb <- ggplot() +
   scale_linetype_manual(values = "longdash") +
     scale_color_manual(values = cols_div[c(6,3)]) +
   theme_bw(base_size = 12) +
-  theme(panel.grid = element_blank()) +
-  guides(color = "none", lty = "none")
+  theme(panel.grid = element_blank(),
+        legend.title = element_blank(),
+        legend.background = element_blank(),
+        # legend.spacing.y = unit(0.05, "cm"),
+        legend.margin = margin(t = 0.025, unit = "cm"),
+        # legend.position.inside = c(0.5, 0.9),
+        legend.position = "top"
+        ) +
+  guides(lty = "none",
+         color = guide_legend(override.aes = list(linetype = c(0, 0)),
+                              byrow = TRUE,
+                              keywidth = 0.1,
+                              keyheight = 0.01,
+                              default.unit = "cm"))
+figb
 
 row1 <- cowplot::plot_grid(figa, figb, nrow = 1,
-                           rel_widths = c(2, 1),
+                           rel_widths = c(2.25, 1),
                            labels = c("a", "b"))
 row1
 
@@ -271,10 +316,11 @@ figc <- ggplot() +
   scale_y_continuous(expression(paste("ET (mmol ", H[2], O, " ", m^-2, s^-1, ")"))) +
   scale_x_reverse(expression(paste(Psi[soil], " (MPa)"))) +
   scale_linetype_manual(values = c("longdash", "solid")) +
-  facet_wrap2(~phase,
+  facet_grid2(~phase,
               strip = strip,
-              # space = "free_x",
+              space = "free_x",
               scales = "free_x") +
+  force_panelsizes(cols = c(1,1.75)) +
   theme_bw(base_size = 12) +
   theme(panel.grid = element_blank(),
         strip.background = element_blank(),
@@ -300,11 +346,23 @@ figd <- ggplot() +
   scale_linetype_manual(values = "solid") +
   scale_color_manual(values = cols_div[c(6,3)]) +
   theme_bw(base_size = 12) +
-  theme(panel.grid = element_blank()) +
-  guides(color = "none", lty = "none")
+  theme(panel.grid = element_blank(),
+        legend.title = element_blank(),
+        legend.background = element_blank(),
+        # legend.spacing.y = unit(0.05, "cm"),
+        legend.margin = margin(t = 0.025, unit = "cm"),
+        # legend.position.inside = c(0.5, 0.9),
+        legend.position = "top"
+  ) +
+  guides(lty = "none",
+         color = guide_legend(override.aes = list(linetype = c(0, 0)),
+                              byrow = TRUE,
+                              keywidth = 0.1,
+                              keyheight = 0.01,
+                              default.unit = "cm"))
 
 row2 <- cowplot::plot_grid(figc, figd, nrow = 1,
-                           rel_widths = c(2, 1),
+                           rel_widths = c(2.25, 1),
                            labels = c("c", "d"))
 row2
 
@@ -369,7 +427,8 @@ fige <- ggplot() +
   facet_wrap2(~phase,
               strip = strip,
               # space = "free_x",
-              scales = "free_x") +
+              scales = "free_x") +  
+  force_panelsizes(cols = c(1,1.75)) +
   theme_bw(base_size = 12) +
   theme(panel.grid = element_blank(),
         strip.background = element_blank(),
@@ -395,11 +454,23 @@ figf <- ggplot() +
   scale_linetype_manual(values = "longdash") +
   scale_color_manual(values = cols_div[c(6,3)]) +
   theme_bw(base_size = 12) +
-  theme(panel.grid = element_blank()) +
-  guides(color = "none", lty = "none")
+  theme(panel.grid = element_blank(),
+        legend.title = element_blank(),
+        legend.background = element_blank(),
+        # legend.spacing.y = unit(0.05, "cm"),
+        legend.margin = margin(t = 0.025, unit = "cm"),
+        # legend.position.inside = c(0.5, 0.9),
+        legend.position = "top"
+  ) +
+  guides(lty = "none",
+         color = guide_legend(override.aes = list(linetype = c(0, 0)),
+                              byrow = TRUE,
+                              keywidth = 0.1,
+                              keyheight = 0.01,
+                              default.unit = "cm"))
 
 row3 <- cowplot::plot_grid(fige, figf, nrow = 1,
-                           rel_widths = c(2, 1),
+                           rel_widths = c(2.25, 1),
                            labels = c("e", "f"))
 row3
 
@@ -412,13 +483,13 @@ fig_7 <- cowplot::plot_grid(figa, figb,
                             fige, figf,
                             nrow = 3,
                             align = "hv", axis = "lb",
-                            rel_widths = c(1.8, 1),
+                            rel_widths = c(2.25, 1),
                             labels = "auto")
 fig_7
 
 ggsave(filename = "fig_scripts/round2/fig7.png",
        plot = fig_7,
-       height = 8,
+       height = 7,
        width = 8,
        units = "in")
 
